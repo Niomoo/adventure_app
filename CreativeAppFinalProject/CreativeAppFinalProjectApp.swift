7000bec9e7667fb4f8b5fc42e80f168f45686fba
Wiily Yin <willy19981031@gmail.com>
2022-06-03 10:48:57 +0800

    Initial Commit

---
+++ CreativeAppFinalProject/CreativeAppFinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreativeAppFinalProjectApp.swift
+//  CreativeAppFinalProject
+//
+//  Created by 尹子維 on 2022/6/3.
+//
+
+import SwiftUI
+
+@main
+struct CreativeAppFinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
